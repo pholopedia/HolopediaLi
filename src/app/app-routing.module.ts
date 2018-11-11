a27dc14b089ebc1bb04400093d4639a648daf65a
omki <aagaardmikkeli@gmail.com>
2018-11-11 17:57:59 +0100

    Initial commit with topbar and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { HomeComponent } from './components/pages/home/home.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { SocialComponent } from './components/pages/social/social.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { GamesComponent } from './components/pages/games/games.component';
+
+const routes: Routes = [
+  { path: 'home',  component: HomeComponent },
+  { path: 'contact',  component: ContactComponent },
+  { path: 'social',  component: SocialComponent },
+  { path: 'projects',  component: ProjectsComponent },
+  { path: 'games',  component: GamesComponent },
+  // individual holo master coin
+  { path: 'projects/:project',  component: ProjectsComponent },
+  // Default
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { CoinComponent } from './components/elements/coin/coin.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { SocialComponent } from './components/pages/social/social.component';
+import { ProjectsComponent } from './components/pages/projects/projects.component';
+import { GamesComponent } from './components/pages/games/games.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    NavigationComponent,
+    CoinComponent,
+    HomeComponent,
+    ContactComponent,
+    SocialComponent,
+    ProjectsComponent,
+    GamesComponent
   ],
   imports: [
     BrowserModule,
