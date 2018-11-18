ceed5a08976e32aef999120c12755a60a0038af8
omki <aagaardmikkeli@gmail.com>
2018-11-18 19:30:58 +0100

    firestore general service

--- src/app/services/holograms/holograms.service.ts
+++ src/app/services/holograms/holograms.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-import { map, filter } from 'rxjs/operators';
 import { Hologram } from './hologram.model';
+import { FirestoreService } from '../firestore/firestore.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,46 +11,35 @@ export class HologramsService {
 
   collection: AngularFirestoreCollection<Hologram>;
   holograms: Observable<Hologram[]>;
-  document: AngularFirestoreDocument<Hologram>;
   afs$: AngularFirestore;
-  collectionName: string = 'holograms';
+  hologramsCollectionName: string = 'holograms';
+  categoriesCollectionName: string = 'hologramCategories';
+  isFullscreen: boolean = false;
 
   constructor(
     public afs: AngularFirestore,
+    public firestoreService: FirestoreService
   ) {
     this.afs$ = afs;
     this.get();
   }
 
   get() {
-    this.collection = this.afs$.collection<Hologram>(this.collectionName);
-    this.holograms = this.collection.snapshotChanges().pipe(map(changes => {
-      return changes.map(a => {
-        const data = a.payload.doc.data() as Hologram;
-        data.id = a.payload.doc.id;
-        return data;
-      })
-    }));
+    this.collection = this.afs$.collection<any>(this.hologramsCollectionName);
+    this.holograms = this.firestoreService.get(this.collection);
     return this.holograms;
   }
 
   add(hologram: Hologram){
-    let promise = new Promise((resolve, reject) => {
-      this.collection.add(hologram)
-      .then((data) => { resolve(data); })
-      .catch(error => reject(error) );
-    });
-    return promise;
+    return this.firestoreService.add(this.collection, hologram);
   }
 
   update(hologram: Hologram) {
-    this.document = this.afs.doc<Hologram>(this.collectionName + "/" + hologram.id);
-    this.document.update(hologram);
+    this.firestoreService.update(hologram, this.hologramsCollectionName);
   }
 
   delete(hologram: Hologram){
-    this.document = this.afs.doc<Hologram>(this.collectionName + "/" + hologram.id);
-    this.document.delete();
+    this.firestoreService.delete(hologram, this.hologramsCollectionName);
   }
 
 }
