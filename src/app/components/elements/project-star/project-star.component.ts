8a522dcdb7091a635d18632ed8a76f89f739a32b
omki <aagaardmikkeli@gmail.com>
2020-06-16 16:59:56 +0200

    holo

--- src/app/components/elements/project-star/project-star.component.ts
+++ src/app/components/elements/project-star/project-star.component.ts
@@ -19,32 +19,32 @@ export class ProjectStarComponent implements OnInit {
   <br> HoloResearch`
 
   coins: CoinSettings[] = [
-    new CoinSettings(["Research"], { theme:  "purple" }, [], { url: this.getFctcoinUrl(".2") }),
-    new CoinSettings(["Education", "Emulation"], { theme: "indigo" }, [], { url: this.getFctcoinUrl(".5") }),
-    new CoinSettings(["Medicine"], { theme: "red" }, [], { url: this.getFctcoinUrl("10") }),
+    new CoinSettings(["Holo Research"], { theme:  "purple" }, [], { url: this.getFctcoinUrl(".2") }),
+    new CoinSettings(["Holo Education", "Holo Emulation"], { theme: "indigo" }, [], { url: this.getFctcoinUrl(".5") }),
+    new CoinSettings(["Holo Medicine"], { theme: "red" }, [], { url: this.getFctcoinUrl("10") }),
     new CoinSettings(["Holo Master"], { theme: "beige" }, [], { url: "assets/images/coin/HP_Holopedia.png" }),
-    new CoinSettings(["Technology", "Therapies", "Tunnels", "Tomography"], { theme: "turquoise" }, [], { url: this.getFctcoinUrl("1") }),
-    new CoinSettings(["Artificial Intelligence", "Applications", "Algorithm"], { theme: "orange" }, [], { url: this.getFctcoinUrl("5") }),
-    new CoinSettings(["Smart", "Sustainability", "Systems", "Simulations"], { theme: "olive" }, [], { url: this.getFctcoinUrl("2") }),
+    new CoinSettings(["Holo Technology", "Holo Therapies", "Holo Tunnels", "Holo Tomography"], { theme: "turquoise" }, [], { url: this.getFctcoinUrl("1") }),
+    new CoinSettings(["Holo Artificial Intelligence", "Holo Applications", "Holo Algorithm"], { theme: "orange" }, [], { url: this.getFctcoinUrl("5") }),
+    new CoinSettings(["Holo Smart", "Holo Sustainability", "Holo Systems", "Holo Simulations"], { theme: "olive" }, [], { url: this.getFctcoinUrl("2") }),
   ]
 
   projects = [
     {name: "Put in Your Hologram for Holopedia HPA",   style: { top: "20px",   left: "50%",                transform: "translateX(-50%)"}},
-    {name: "Medicine",                                style: { top: "64px",   left: "calc(50% - 97px)",   transform: "translateX(-100%)"}},
-    {name: "Artificial Intelligence",                 style: { top: "123px",  left: "calc(50% - 190px)",  transform: "translateX(-100%)"}},
-    {name: "Applications",                            style: { top: "216px",  left: "calc(50% - 246px)",  transform: "translateX(-100%)"}},
-    {name: "Algorithms",                              style: { top: "320px",  left: "calc(50% - 266px)",  transform: "translateX(-100%)"}},
-    {name: "Smart",                                   style: { top: "424px",  left: "calc(50% - 246px)",  transform: "translateX(-100%)"}},
-    {name: "Sustainability",                          style: { top: "516px",  left: "calc(50% - 190px)",  transform: "translateX(-100%)"}},
-    {name: "Systems",                                 style: { top: "574px",  left: "calc(50% - 97px)",   transform: "translateX(-100%)"}},
-    {name: "Simulations",                             style: { top: "624px",  left: "50%",                transform: "translateX(-50%)"}},
-    {name: "Technology",                              style: { top: "574px",  left: "calc(50% + 97px)"}},
-    {name: "Therapies",                               style: { top: "516px",  left: "calc(50% + 190px)"}},
-    {name: "Tunnels",                                 style: { top: "424px",  left: "calc(50% + 246px)"}},
-    {name: "Tomography",                              style: { top: "320px",  left: "calc(50% + 266px)"}},
-    {name: "Education",                               style: { top: "216px",  left: "calc(50% + 246px)"}},
-    {name: "Emulation",                               style: { top: "123px",  left: "calc(50% + 190px)"}},
-    {name: "Research",                                style: { top: "64px",   left: "calc(50% + 97px)"}},
+    {name: "Holo Medicine",                                style: { top: "64px",   left: "calc(50% - 97px)",   transform: "translateX(-100%)"}},
+    {name: "Holo Artificial Intelligence",                 style: { top: "123px",  left: "calc(50% - 190px)",  transform: "translateX(-100%)"}},
+    {name: "Holo Applications",                            style: { top: "216px",  left: "calc(50% - 246px)",  transform: "translateX(-100%)"}},
+    {name: "Holo Algorithms",                              style: { top: "320px",  left: "calc(50% - 266px)",  transform: "translateX(-100%)"}},
+    {name: "Holo Smart",                                   style: { top: "424px",  left: "calc(50% - 246px)",  transform: "translateX(-100%)"}},
+    {name: "Holo Sustainability",                          style: { top: "516px",  left: "calc(50% - 190px)",  transform: "translateX(-100%)"}},
+    {name: "Holo Systems",                                 style: { top: "574px",  left: "calc(50% - 97px)",   transform: "translateX(-100%)"}},
+    {name: "Holo Simulations",                             style: { top: "624px",  left: "50%",                transform: "translateX(-50%)"}},
+    {name: "Holo Technology",                              style: { top: "574px",  left: "calc(50% + 97px)"}},
+    {name: "Holo Therapies",                               style: { top: "516px",  left: "calc(50% + 190px)"}},
+    {name: "Holo Tunnels",                                 style: { top: "424px",  left: "calc(50% + 246px)"}},
+    {name: "Holo Tomography",                              style: { top: "320px",  left: "calc(50% + 266px)"}},
+    {name: "Holo Education",                               style: { top: "216px",  left: "calc(50% + 246px)"}},
+    {name: "Holo Emulation",                               style: { top: "123px",  left: "calc(50% + 190px)"}},
+    {name: "Holo Research",                                style: { top: "64px",   left: "calc(50% + 97px)"}},
   ]
   
 
