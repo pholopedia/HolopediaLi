a380c61743ddc96998c69ff16f3949eb75c21c4c
Mikkel Aagaard <maa@simplesite.com>
2020-06-19 20:05:35 +0200

    3D technologies on club

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -11,6 +11,8 @@ export class HomeComponent implements OnInit {
 
   projectsNames: string[];
   sourcesNames: string[];
+  categories;
+  technologies;
 
   constructor(
     public hologramsService: HologramsService,
@@ -21,6 +23,20 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     this.projectsNames = this.projectsService.MasterProjectsNames;
     this.sourcesNames = this.hologramsService.Sources;
+
+    this.categories = [
+      { name: "SPA Massage", imageFile: "massage_stones.png" },
+      { name: "Treatments", imageFile: "biofeedback.png" },
+      { name: "Knowledge", imageFile: "knowledge.png" },
+      { name: "Zuberec", imageFile: "zuberec.jpg" },
+    ]
+
+    this.technologies = [
+      { name: "Holopedia 3D LED fans", imageFile: "holopedia_3D_LED_fans.png" },
+      { name: "Holopedia VR", imageFile: "holopedia_vr.png" },
+      { name: "Holopedia Pyramid", imageFile: "holopedia_pyramid.png" },
+      { name: "Holopedia Looking Glass", imageFile: "holopedia_looking_glass.png" },
+    ]
   }
 
 }
