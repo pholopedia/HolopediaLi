4bebaf43d02df287aa869574696bdfd56101210c
Mikkel <aagaardmikkeli@gmail.com>
2020-09-29 02:27:28 +0200

    sign in form

--- src/app/components/whitepaper/whitepaper.component.ts
+++ src/app/components/whitepaper/whitepaper.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-whitepaper',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WhitepaperComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public auth: AuthService
+  ) { 
+    
+  }
 
   ngOnInit() {
   }
